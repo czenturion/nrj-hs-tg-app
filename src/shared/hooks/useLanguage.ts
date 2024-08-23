26f34d70274452a64aa0247a262b97687a040f17
Maks Arefev <czenturijon@gmail.com>
2024-08-24 00:19:50 +0300

    fix: lang init wrong

--- src/shared/hooks/useLanguage.ts
+++ src/shared/hooks/useLanguage.ts
@@ -1,46 +1,46 @@
 import { useState, useEffect } from 'react'
 import i18n from '../../i18n'
 
+const langHandler = async (initialLanguage: string, setLanguage: (s: string) => void) => {
+  try {
+    await i18n.changeLanguage(initialLanguage)
+    setLanguage(initialLanguage)
+  } catch (e) {
+    console.error('Failed to change language:', e)
+  }
+}
+
 export const useLanguage = () => {
   const [language, setLanguage] = useState('en')
 
   useEffect(() => {
     const tg = window.Telegram.WebApp
 
-    if (tg) {
-      const userLang = tg.initDataUnsafe.user?.language_code || 'en'
-      const initialLanguage = userLang === 'ru' ? 'ru' : 'en'
+    const initializeLanguage = async () => {
+      if (tg) {
+        const userLang = tg.initDataUnsafe.user?.language_code || 'en'
+        const initialLanguage = userLang === 'ru' ? 'ru' : 'en'
 
-      i18n.changeLanguage(initialLanguage)
-        .then(() => {
-          setLanguage(initialLanguage)
-        })
-        .catch((e) => {
-          console.log('Failed to change language: ', e)
-        })
+        await langHandler(initialLanguage, setLanguage)
 
-      const handleLanguageChange = (lng: string) => {
-        setLanguage(lng)
-      }
+        const handleLanguageChange = (lng: string) => {
+          setLanguage(lng);
+        }
 
-      i18n.on('languageChanged', handleLanguageChange)
+        i18n.on('languageChanged', handleLanguageChange)
 
-      return () => {
-        i18n.off('languageChanged', handleLanguageChange)
+        return () => {
+          i18n.off('languageChanged', handleLanguageChange)
+        }
       }
     }
+
+    initializeLanguage()
   }, [])
 
-  const toggleLanguage = () => {
+  const toggleLanguage = async () => {
     const newLanguage = language === 'ru' ? 'en' : 'ru'
-
-    i18n.changeLanguage(newLanguage)
-      .then(() => {
-        setLanguage(newLanguage)
-      })
-      .catch((e) => {
-        console.log('Failed to change language: ', e)
-      })
+    await langHandler(newLanguage, setLanguage)
   }
 
   return { language, toggleLanguage }
